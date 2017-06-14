0e1d0dd201b52fa63293e993800a4c1f42e8929e
Pierre Aubin <aubinp1306@gmail.com>
2017-06-14 11:36:24 +0200

    Ajout index.php

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 require_once 'myPDO.class.php';
 require_once 'webpage.class.php';
 require_once 'Eleve.class.php';
-//require_once 'navbar.php';
+require_once 'navbar.php';
 
 $w = new Webpage('Accueil');
 $w->setTitle('Administration');
 $w->appendCssURL('bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css');
 $w->appendCssURL('font-awesome-4.7.0/css/font-awesome.min.css');
-$w->appendCssURL('css/style.css');
+$w->appendCssURL('css/style-accueil.css');
 $w->appendToHead('<link rel="icon" type="image/png" href="img/favicon.png" />');     
-$w->appendContent(NAVBAR);
+$w->appendContent(navbar());
 $w->appendJsURL('https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"');
 $w->appendJsURL('https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"');
 $w->appendJsURL('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"');
@@ -66,12 +66,15 @@ SQL;
         }
 
 
-$mois = array(01 => "janvier", 02 => "février", 03 => "mars", 04 => "avril", 05 => "mai", 06 => "juin", 07 => "juillet", 08 => "aout", 09 => "septembre", 10 => "octobre", 11 => "novembre", 12 => "decembre");      
+$mois = array(1 => "janvier", 2 => "février", 3 => "mars", 4 => "avril", 5 => "mai", 6 => "juin", 7 => "juillet", 8 => "aout", 9 => "septembre", 10 => "octobre", 11 => "novembre", 12 => "decembre");      
 
     
 for ($i = 0; $i<count($eleves); $i++) {
-    $html .= '<p>' . $eleves[$i]->getNom() . " " . $eleves[$i]->getPrenom() . " - Anniversaire le " . substr($eleves[$i]->getDateNaissance(),0,2) . " " . $mois[substr($eleves[$i]->getDateNaissance(),2,2)] . "</p>"
-        . $mois[01];
+    $month = substr($eleves[$i]->getDateNaissance(),2,2);
+    if ($month == 01 || $month == 02 || $month == 03 || $month == 04 || $month == 05 ||$month == 06 || $month == '07' ||$month == 08 || $month == 09) {
+        $month = substr($month,1,1);
+    }
+    $html .= '<p>' . $eleves[$i]->getNom() . " " . $eleves[$i]->getPrenom() . " - Anniversaire le " . substr($eleves[$i]->getDateNaissance(),0,2) . " " . $mois[$month] . "</p>";
 }
 
 $html .= '
@@ -79,14 +82,14 @@ $html .= '
                 </div>
             </div>
         </div>
-
-        <footer class="footer footer-inverse bg-inverse">
-            <div class="container">
-                <span class="text-muted">Salut bande de petits zboub</span>
+        <footer class="footer footer-inverse bg-inverse text-muted">
+            <div class="row">
+                <span class="offset-sm-1 col-sm-3">© Copyright 2016/2017 <strong>I.U.T Reims-Châlons-Charleville</strong></span>
+                <span class="offset-sm-1 col-sm-3">Chemin des Rouliers / CS30012 / 51687 REIMS CEDEX 2</span>
+                <a class="offset-sm-1 col-sm-3" href="http://www.univ-reims.fr/universite/organisation/organisation,7741,18258.html?&args=v6c8KtUbgtua5qBgA22_BFRfK6yFIeIoj8EcmN3JzmQeiUvNoRi1XBSVepe_gwJU4uFsUzkOJdUDlLJudc8M3g" target="_blank"><strong><i class="fa fa-phone-square" aria-hidden="true"></i> Nous contacter</strong></a>
             </div>
         </footer>
 ';
 
 $w->appendContent($html);
-$w->appendContent(FOOTER);
 echo ($w->toHTML());
--- navbar.php
+++ navbar.php
@@ -1,31 +1,29 @@
 <?php
-require_once "webpage.class.php"
+require_once "webpage.class.php";
 
 function navbar() {
 
-  $navbar = "
-  <nav class='navbar navbar-toggleable-md navbar-inverse bg-inverse'>
-      <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarTogglerDemo01' aria-controls='navbarTogglerDemo01' aria-expanded='false' aria-label='Toggle navigation'>
-          <span class='navbar-toggler-icon'></span>
-      </button>
-      <div class='collapse navbar-collapse' id='navbarTogglerDemo01'>
-          <a class='navbar-brand' href='#''>Accueil</a>
-          <ul class='navbar-nav mr-auto mt-2 mt-lg-0'>
-              <li class='nav-item active'>
-                  <a class='nav-link' href='#'><i class='fa fa-star' aria-hidden='true'></i> Pannel Admin</a>
-              </li>
-          </ul>
-      </div>
-    "
+  $navbar = '<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <a class="navbar-brand" href="#">Accueil</a>
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#"><i class="fa fa-star" aria-hidden="true"></i> Pannel Admin</a>
+                    </li>
+                </ul>
+            </div>';
 
-    if( isset(isset($_cookie["prof"]) )) {
+    if( isset($_cookie["prof"]) ) {
       $navbar .=
         "  <div class='dropdown navbar-toggler-right'>
                 <span> Bonjour " .
                 $_cookie["prof"][0]->getPrenom()
                 . " ! </span>
           </div>
-        <nav>
+        </nav>
         "
         ;
 
@@ -34,25 +32,24 @@ function navbar() {
 
     else {
      $navbar .=
-     "  <div class='dropdown navbar-toggler-right'>
-          <button class='btn btn-success dropdown-toggle' type='button' id='dropdownconnect' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-              <i class='fa fa-user-circle-o' aria-hidden='true'></i> Se connecter
-          </button>
-          <div class='dropdown-menu dropdown-menu-right' aria-labelledby='dropdownconnect'>
-              <div class='container-fluid'>
-                  <form>
-                      <div class='form-group row' action='cookieConnection.php' method='POST'>
-                          <input class='form-control offset-sm-1 col-sm-10 offset-sm-1' type='text' placeholder='Utilisateur' name='id' required>
-                          <input class='form-control offset-sm-1 col-sm-10 offset-sm-1' type='password' placeholder='Mot de passe' name='password' required>
-                          <button class='btn btn-success  offset-sm-1 col-sm-10 offset-sm-1' type='submit'>Connexion</button>
-                      </div>
-                  </form>
-              </div>
-          </div>
-      </div>
-  </nav>
-  " ;
+     '  <div class="dropdown navbar-toggler-right">
+                <button class="btn btn-success dropdown-toggle" type="button" id="dropdownconnect" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fa fa-user-circle-o" aria-hidden="true"></i> Se connecter
+                </button>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownconnect">
+                    <div class="container-fluid">
+                        <form>
+                            <div class="form-group row">
+                                <input class="form-control offset-sm-1 col-sm-10 offset-sm-1" type="text" placeholder="Utilisateur" required>
+                                <input class="form-control offset-sm-1 col-sm-10 offset-sm-1" type="password" placeholder="Mot de passe" required>
+                                <button class="btn btn-success  offset-sm-1 col-sm-10 offset-sm-1" type="submit">Connexion</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+  ' ;
 }
 
-  return $navbar ;
+  return $navbar.'</nav>' ;
 }
