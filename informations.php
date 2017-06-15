e275a041f2327e230f4f0e01d317abdeb5828abc
Sylvain COMBRAQUE <sylvaincombraque@hotmail.fr>
2017-06-15 14:29:46 +0200

    Informations done

--- informations.php
+++ informations.php
@@ -16,6 +16,38 @@ $w->appendJsURL('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/boot
 $w->appendJsURL('js/javascript.js');
 
 $html =<<<HTML
+<div class="row">
+    <div class="col-md-12" style="height: 50px;"></div>
+    <div class="col-sm-12">
+        <div class="alert alert-info offset-md-2 col-md-8 offset-md-2 text-center" role="alert">
+            Informations relatives à l'école primaire du pré vers l'Aisne
+        </div>
+    </div>
+    <div class="col-md-12" style="height: 50px;"></div>
+</div>
+<div class="row">
+    <div class="col-md-6 col-sm-12">
+        <div class="card text-center">
+            <div class="card-header">
+               <i class="fa fa-address-book fa-2x" aria-hidden="true"></i><strong> Nous contacter :</strong>
+            </div>
+            <div class="card-block">
+                <div class="col-sm-6">
+                    <div class="text-left"><i class="fa fa-question-circle fa-2x"></i><span> Type d'école : Publique</span></div>
+                    <div class="text-left"><i class="fa fa-search fa-2x"></i><span> Zone : B</span></div>
+                    <div class="text-left"><i class="fa fa-globe fa-2x"></i><span> Adresse : 1 rue de la Barre</span></div>
+                    <div class="text-left"><i class="fa fa-location-arrow fa-2x"></i><span> Code postal : 08190</span></div>
+                    <div class="text-left"><i class="fa fa-thumb-tack fa-2x"></i><span> Ville : Asfeld</span></div>
+                    <div class="text-left"><i class="fa fa-phone fa-2x"></i><span> Téléphone : 03 24 72 94 66</span></div>
+                    <div class="text-left"><i class="fa fa-envelope fa-2x"></i><span> Email : </span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6 col-sm-12" id="card2">
+        <iframe src="http://cache.media.education.gouv.fr/lib/men/pdf/GenerationPDF.php?url=http%253A%252F%252Fwww.education.gouv.fr%252Fexports%252FannuaireOTS.php%253Fuai%253D0080545Z%2526onglet_ots%253Dmaternelle&file_name=ots_maternelle_Asfeld_08190&options=%5B%22--footer-html%2B%2526quot%253Bhttp%253A%252F%252Fwww.education.gouv.fr%252Fexports%252FannuaireOTS_footer.php%2526quot%253B%22%2C%22--margin-bottom%2B95%22%2C%22--margin-left%2B5%22%2C%22--margin-right%2B5%22%2C%22--margin-top%2B10%22%5D" class="col-sm-12" height="100%" align="middle"></iframe>
+    </div>
+</div>
 HTML;
 
 
