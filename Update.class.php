13209b591396917cfe236b06ac12b340739369d1
Sylvain COMBRAQUE <sylvaincombraque@hotmail.fr>
2017-06-15 15:18:18 +0200

    ah*

---
+++ Update.class.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 15/06/17
+ * Time: 15:10
+ */
+
+class Update{
+
+    public static function updateStudent($id, $chp, $value){
+        if()
+
+        $requete =<<<SQL
+UPDATE ELEVE SET $chp = $value
+SQL;
+
+    }
+
+    public static function updateProf(){
+
+    }
+
+    public static function updateObs(){
+
+    }
+
+    public static function updateClass(){
+
+    }
+
+    public static function updateCategory(){
+
+    }
+
+}
