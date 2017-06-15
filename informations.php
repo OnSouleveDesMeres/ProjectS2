308da197a323cf1bc01c83482810d64daa6fa64d
Alexandre Perseval <alexandre.perseval@etudiant.univ-reims.fr>
2017-06-15 11:42:47 +0200

    Debut page info

--- informations.php
+++ informations.php
@@ -1,1 +1,21 @@
-<img src="http://wwwdoc/photos/photo.php?idpers=2487">
+<?php
+require_once 'webpage.class.php';
+require_once 'navbar.php';
+require_once 'footer.php';
+
+$w = new Webpage('Informations');
+
+$w->appendCssURL('bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css');
+$w->appendCssURL('font-awesome-4.7.0/css/font-awesome.min.css');
+$w->appendCssURL('css/style-accueil.css');
+$w->appendToHead('<link rel="icon" type="image/png" href="img/favicon.png" />');
+$w->appendContent(navbar());
+$w->appendJsURL('https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"');
+$w->appendJsURL('https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"');
+$w->appendJsURL('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"');
+$w->appendJsURL('js/javascript.js');
+
+$html = '<img src="http://wwwdoc/photos/photo.php?idpers=2487">';
+$w->appendContent($html);
+$w->appendContent(footer());
+echo ($w->toHTML());
--- insertClass.php
+++ insertClass.php
@@ -1,4 +1,10 @@
-<?php
+<?php<?php
+require_once 'myPDO.class.php';
+require_once 'webpage.class.php';
+require_once 'Eleve.class.php';
+require_once 'navbar.php';
+require_once 'footer.php';
+require_once 'Classe.class.php';
 /**
  * Created by PhpStorm.
  * User: root
