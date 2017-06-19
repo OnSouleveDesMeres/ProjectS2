d4e33e79f593948558b92485bd60b25ba457149f
Sylvain COMBRAQUE <sylvaincombraque@hotmail.fr>
2017-06-19 11:42:02 +0200

    Redirections

--- changePassword.php
+++ changePassword.php
@@ -9,29 +9,30 @@ require_once 'updatePassword.php';
 require_once 'webpage.class.php';
 require_once 'navbar.php';
 require_once 'footer.php';
+if(isset($_COOKIE["profFirstName"]) && !empty($_COOKIE["profFirstName"]) && isset($_COOKIE["profId"]) && !empty($_COOKIE["profId"])) {
+    $w = new Webpage('Modification de mot de passe');
+    $w->appendCssURL('bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css');
+    $w->appendCssURL('font-awesome-4.7.0/css/font-awesome.min.css');
+    $w->appendCssURL('css/style-accueil.css');
+    $w->appendToHead('<link rel="icon" type="image/png" href="img/favicon.png" />');
+    $w->appendContent(navbar());
+    $w->appendJsURL('https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"');
+    $w->appendJsURL('https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"');
+    $w->appendJsURL('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"');
+    $w->appendJsURL('js/javascript.js');
 
-$w = new Webpage('Modification de mot de passe');
-$w->appendCssURL('bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css');
-$w->appendCssURL('font-awesome-4.7.0/css/font-awesome.min.css');
-$w->appendCssURL('css/style-accueil.css');
-$w->appendToHead('<link rel="icon" type="image/png" href="img/favicon.png" />');
-$w->appendContent(navbar());
-$w->appendJsURL('https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"');
-$w->appendJsURL('https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"');
-$w->appendJsURL('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"');
-$w->appendJsURL('js/javascript.js');
+    $successorfail = '';
 
-$successorfail ='';
+    if (isset($_COOKIE['profId']) && !empty($_COOKIE['profId']) &&
+        isset($_POST['lastpass']) && !empty($_POST['lastpass']) &&
+        isset($_POST['newpass']) && !empty($_POST['newpass'])
+    ) {
 
-if (isset($_COOKIE['profId']) && !empty($_COOKIE['profId']) &&
-    isset($_POST['lastpass']) && !empty($_POST['lastpass']) &&
-    isset($_POST['newpass']) && !empty($_POST['newpass'])){
+        $successorfail = changePassword($_COOKIE['profId'], $_POST['lastpass'], $_POST['newpass']);
 
-    $successorfail = changePassword($_COOKIE['profId'], $_POST['lastpass'], $_POST['newpass']);
+    }
 
-}
-
-$html =<<<HTML
+    $html = <<<HTML
 <div class="col-md-12 text-center center">
     <div class="col-md-12" style="height:50px;"></div>
     {$successorfail}
@@ -59,7 +60,13 @@ $html =<<<HTML
 HTML;
 
 
-$w->appendContent($html);
+    $w->appendContent($html);
 
-$w->appendContent(footer());
-echo $w->toHTML();
+    $w->appendContent(footer());
+    echo $w->toHTML();
+}
+else{
+
+    header("Location: index.php");
+
+}
--- observable.php
+++ observable.php
@@ -5,26 +5,27 @@ require_once 'footer.php';
 require_once 'Observable.class.php';
 require_once 'Categorie.class.php';
 
-$w = new Webpage('Eleves');
+if(isset($_COOKIE["profFirstName"]) && !empty($_COOKIE["profFirstName"]) && isset($_COOKIE["profId"]) && !empty($_COOKIE["profId"])) {
+    $w = new Webpage('Eleves');
 
-$w->appendCssURL('bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css');
-$w->appendCssURL('font-awesome-4.7.0/css/font-awesome.min.css');
-$w->appendCssURL('css/style-accueil.css');
-$w->appendToHead('<link rel="icon" type="image/png" href="img/favicon.png" />');
-$w->appendContent(navbar());
-$w->appendJsURL('https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"');
-$w->appendJsURL('https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"');
-$w->appendJsURL('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"');
-$w->appendJsURL('js/javascript.js');
+    $w->appendCssURL('bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css');
+    $w->appendCssURL('font-awesome-4.7.0/css/font-awesome.min.css');
+    $w->appendCssURL('css/style-accueil.css');
+    $w->appendToHead('<link rel="icon" type="image/png" href="img/favicon.png" />');
+    $w->appendContent(navbar());
+    $w->appendJsURL('https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"');
+    $w->appendJsURL('https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"');
+    $w->appendJsURL('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"');
+    $w->appendJsURL('js/javascript.js');
 
-$html='';
+    $html = '';
 
-if(isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $observable = Observable::createFromId($id);
-    $Categorie = Categorie::createFromId($observable[0]->getIdCatg());
-    $Categories = Categorie::getAll();
-    $html .= <<<HTML
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $observable = Observable::createFromId($id);
+        $Categorie = Categorie::createFromId($observable[0]->getIdCatg());
+        $Categories = Categorie::getAll();
+        $html .= <<<HTML
     <div style="height:15px;"></div>
     <form name="Observable" method="GET" action="updateObservable.php">
 
@@ -40,11 +41,11 @@ if(isset($_GET['id'])) {
 			    <select class="form-control" name="categorie" id="categorie" value="{$Categorie[0]->getNom()}">
 			    	<option value="{$Categorie[0]->getId()}">{$Categorie[0]->getNom()}</option>
 HTML;
-		foreach($Categories as $c) {
-			$html .= '<option value="' . $c->getId() . '">' . $c->getNom() . '</option>';
-		}
+        foreach ($Categories as $c) {
+            $html .= '<option value="' . $c->getId() . '">' . $c->getNom() . '</option>';
+        }
 
-$html .= <<<HTML
+        $html .= <<<HTML
 			</select>
 		</div>
 		</div>
@@ -60,12 +61,15 @@ $html .= <<<HTML
         </form>
 
 HTML;
+    } else {
+        $html .= '<h1>Erreur observable introuvable</h1>';
+    }
+
+
+    $w->appendContent($html);
+    $w->appendContent(footer());
+    echo($w->toHTML());
 }
 else{
-    $html .= '<h1>Erreur observable introuvable</h1>';
+    header('Location: index.php');
 }
-
-
-$w->appendContent($html);
-$w->appendContent(footer());
-echo ($w->toHTML());
