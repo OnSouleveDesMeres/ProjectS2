38139aec8d09d4ecf3762367c8f62325b4ddbe58
GodFuckABitch <sylvain.combraque@etudiant.univ-reims.fr>
2017-11-23 17:25:45 +0100

    sejkbfqklzjdbqz

--- updateProfil.php
+++ updateProfil.php
@@ -29,11 +29,11 @@ if (isset($_COOKIE["profId"])) {
         {
             $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
         }
-        if($taille>$taille_maxi)
+        elseif($taille>$taille_maxi)
         {
             $erreur = 'Le fichier est trop gros...';
         }
-        if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
+        else //S'il n'y a pas d'erreur, on upload
         {
             //On formate le nom du fichier ici...
             $fichier = $nom.$prenom;
@@ -42,7 +42,7 @@ if (isset($_COOKIE["profId"])) {
                 'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
             $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
             move_uploaded_file($_FILES['avatar']['tmp_name'], $dossier . $fichier);
-            Update::updateProf($_COOKIE['profId'], 'IMGPATH', "{$dossier}{$fichier}");
+            Update::updateProf($_COOKIE['profId'], 'IMGPATH', $dossier . $fichier);
         }
     }
         $requete = <<<SQL
